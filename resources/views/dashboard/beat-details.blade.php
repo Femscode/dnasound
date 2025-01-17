c204648135956a009736eef1c1eba266dc181135
Pelumi <fasanyafemi@gmail.com>
2025-01-17 07:29:07 +0100

    add player

--- resources/views/dashboard/beat-details.blade.php
+++ resources/views/dashboard/beat-details.blade.php
@@ -226,7 +226,7 @@
                         <div class="album album--sidebar">
                             <div class="album__cover">
                                 <img src="img/covers/cover8.jpg" alt="">
-                                <a href="release.html"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                         <path d="M18.54,9,8.88,3.46a3.42,3.42,0,0,0-5.13,3V17.58A3.42,3.42,0,0,0,7.17,21a3.43,3.43,0,0,0,1.71-.46L18.54,15a3.42,3.42,0,0,0,0-5.92Zm-1,4.19L7.88,18.81a1.44,1.44,0,0,1-1.42,0,1.42,1.42,0,0,1-.71-1.23V6.42a1.42,1.42,0,0,1,.71-1.23A1.51,1.51,0,0,1,7.17,5a1.54,1.54,0,0,1,.71.19l9.66,5.58a1.42,1.42,0,0,1,0,2.46Z" />
                                     </svg></a>
                                 <span class="album__stat">
@@ -239,7 +239,7 @@
                                 </span>
                             </div>
                             <div class="album__title">
-                                <h3><a href="release.html">Space Melody</a></h3>
+                                <h3><a href="#">Space Melody</a></h3>
                                 <span><a href="artist.html">VIZE</a> & <a href="artist.html">Alan Walker</a> & <a href="artist.html">Leony</a></span>
                             </div>
                         </div>
@@ -249,7 +249,7 @@
                         <div class="album album--sidebar">
                             <div class="album__cover">
                                 <img src="img/covers/cover2.jpg" alt="">
-                                <a href="release.html"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                         <path d="M18.54,9,8.88,3.46a3.42,3.42,0,0,0-5.13,3V17.58A3.42,3.42,0,0,0,7.17,21a3.43,3.43,0,0,0,1.71-.46L18.54,15a3.42,3.42,0,0,0,0-5.92Zm-1,4.19L7.88,18.81a1.44,1.44,0,0,1-1.42,0,1.42,1.42,0,0,1-.71-1.23V6.42a1.42,1.42,0,0,1,.71-1.23A1.51,1.51,0,0,1,7.17,5a1.54,1.54,0,0,1,.71.19l9.66,5.58a1.42,1.42,0,0,1,0,2.46Z" />
                                     </svg></a>
                                 <span class="album__stat">
@@ -262,7 +262,7 @@
                                 </span>
                             </div>
                             <div class="album__title">
-                                <h3><a href="release.html">Said Sum</a></h3>
+                                <h3><a href="#">Said Sum</a></h3>
                                 <span><a href="artist.html">Moneybagg</a></span>
                             </div>
                         </div>
@@ -272,7 +272,7 @@
                         <div class="album album--sidebar">
                             <div class="album__cover">
                                 <img src="img/covers/cover3.jpg" alt="">
-                                <a href="release.html"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                         <path d="M18.54,9,8.88,3.46a3.42,3.42,0,0,0-5.13,3V17.58A3.42,3.42,0,0,0,7.17,21a3.43,3.43,0,0,0,1.71-.46L18.54,15a3.42,3.42,0,0,0,0-5.92Zm-1,4.19L7.88,18.81a1.44,1.44,0,0,1-1.42,0,1.42,1.42,0,0,1-.71-1.23V6.42a1.42,1.42,0,0,1,.71-1.23A1.51,1.51,0,0,1,7.17,5a1.54,1.54,0,0,1,.71.19l9.66,5.58a1.42,1.42,0,0,1,0,2.46Z" />
                                     </svg></a>
                                 <span class="album__stat">
@@ -285,7 +285,7 @@
                                 </span>
                             </div>
                             <div class="album__title">
-                                <h3><a href="release.html">I Love My Country</a></h3>
+                                <h3><a href="#">I Love My Country</a></h3>
                                 <span><a href="artist.html">Florida Georgia</a></span>
                             </div>
                         </div>
@@ -295,7 +295,7 @@
                         <div class="album album--sidebar">
                             <div class="album__cover">
                                 <img src="img/covers/cover6.jpg" alt="">
-                                <a href="release.html"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                         <path d="M18.54,9,8.88,3.46a3.42,3.42,0,0,0-5.13,3V17.58A3.42,3.42,0,0,0,7.17,21a3.43,3.43,0,0,0,1.71-.46L18.54,15a3.42,3.42,0,0,0,0-5.92Zm-1,4.19L7.88,18.81a1.44,1.44,0,0,1-1.42,0,1.42,1.42,0,0,1-.71-1.23V6.42a1.42,1.42,0,0,1,.71-1.23A1.51,1.51,0,0,1,7.17,5a1.54,1.54,0,0,1,.71.19l9.66,5.58a1.42,1.42,0,0,1,0,2.46Z" />
                                     </svg></a>
                                 <span class="album__stat">
@@ -308,7 +308,7 @@
                                 </span>
                             </div>
                             <div class="album__title">
-                                <h3><a href="release.html">Toosie Slide</a></h3>
+                                <h3><a href="#">Toosie Slide</a></h3>
                                 <span><a href="artist.html">Drake</a></span>
                             </div>
                         </div>
