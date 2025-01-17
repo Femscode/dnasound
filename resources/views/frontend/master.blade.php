5fea7097d168e461ab795fab83a6c45f75954e8e
Pelumi <fasanyafemi@gmail.com>
2025-01-17 07:44:34 +0100

    add player

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -188,21 +188,21 @@
     @yield('content')
 
 
-        <div class="player">
-            <div class="player__cover">
-                <img src="{{asset('dna/img/covers/cover.svg')}}" alt="">
-            </div>
-
-            <div class="player__content">
-                <span class="player__track"><b class="player__title">DNA Music</b> – <span class="player__artist">{{$user->name ?? "Playlist"}}</span></span>
-                <audio src="https://dmitryvolkov.me/demo/blast2.0/audio/12071151_epic-cinematic-trailer_by_audiopizza_preview.mp3" id="audio" controls></audio>
-            </div>
+    <div class="player">
+        <div class="player__cover">
+            <img src="{{asset('dna/img/covers/cover.svg')}}" alt="">
         </div>
 
-        <button class="player__btn" type="button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path d="M21.65,2.24a1,1,0,0,0-.8-.23l-13,2A1,1,0,0,0,7,5V15.35A3.45,3.45,0,0,0,5.5,15,3.5,3.5,0,1,0,9,18.5V10.86L20,9.17v4.18A3.45,3.45,0,0,0,18.5,13,3.5,3.5,0,1,0,22,16.5V3A1,1,0,0,0,21.65,2.24ZM5.5,20A1.5,1.5,0,1,1,7,18.5,1.5,1.5,0,0,1,5.5,20Zm13-2A1.5,1.5,0,1,1,20,16.5,1.5,1.5,0,0,1,18.5,18ZM20,7.14,9,8.83v-3L20,4.17Z" />
-            </svg> Player</button>
-   
+        <div class="player__content">
+            <span class="player__track"><b class="player__title">DNA Music</b> – <span class="player__artist">{{$user->name ?? "Playlist"}}</span></span>
+            <audio src="https://dmitryvolkov.me/demo/blast2.0/audio/12071151_epic-cinematic-trailer_by_audiopizza_preview.mp3" id="audio" controls></audio>
+        </div>
+    </div>
+
+    <button class="player__btn" type="button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M21.65,2.24a1,1,0,0,0-.8-.23l-13,2A1,1,0,0,0,7,5V15.35A3.45,3.45,0,0,0,5.5,15,3.5,3.5,0,1,0,9,18.5V10.86L20,9.17v4.18A3.45,3.45,0,0,0,18.5,13,3.5,3.5,0,1,0,22,16.5V3A1,1,0,0,0,21.65,2.24ZM5.5,20A1.5,1.5,0,1,1,7,18.5,1.5,1.5,0,0,1,5.5,20Zm13-2A1.5,1.5,0,1,1,20,16.5,1.5,1.5,0,0,1,18.5,18ZM20,7.14,9,8.83v-3L20,4.17Z" />
+        </svg> Player</button>
+
     <footer style='background-color: #333333;' class="text-light py-0">
         <div class="container">
             <div class="row">
@@ -228,7 +228,7 @@
             </div>
             <div class="row">
                 <!-- Logo and Short Details -->
-                <div class="col-md-4 mb-4">
+                <div class="col-md-4 mb-4 footer-image">
                     <svg width="352" height="164" viewBox="0 0 352 164" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="176" cy="176.002" r="35" stroke="#6B6B6B" stroke-width="2" />
                         <circle cx="176" cy="176.002" r="58.3333" stroke="#6B6B6B" stroke-width="2" />
@@ -238,9 +238,6 @@
                         <circle cx="176" cy="176.002" r="151.667" stroke="#6B6B6B" stroke-width="2" />
                         <circle cx="176" cy="176.002" r="175" stroke="#6B6B6B" stroke-width="2" />
                     </svg>
-
-
-
 
                 </div>
                 <!-- Quick Links -->
